4f5cf543981624f25955f0898b97788c931330b6
Michael Bragg <michael@michaelbragg.net>
2016-05-16 17:13:10 +0100

    Add hero banner options to pages

---
+++ html/app/plugins/tm-hero.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Plugin Name: TM Hero
+ * Plugin URI:
+ * Description: Adds a Hero banner meta box
+ * Author: Michael Bragg
+ * Author URI:
+ * Version: 0.1.0
+*/
+
+class TM_Hero {
+
+	/**
+	 * Maintain the single instance of TM_Hero
+	 *
+	 * @var bool
+	 */
+	private static $instance = false;
+
+	protected $user;
+
+	/**
+	 * Add required hooks
+	 */
+	function __construct() {
+
+		// Get current users details
+		add_action(
+			'init',
+			array( $this, 'tm_hero_get_user' )
+		);
+
+		// Run actions once WordPress has initialized
+		add_action(
+			'init',
+			array( $this, 'tm_hero_init' )
+		);
+
+	}
+
+	/**
+	 * Handle requests for the instance.
+	 *
+	 * @return bool
+	 */
+	public static function get_instance() {
+		if ( ! self::$instance ) {
+			self::$instance = new TM_Hero();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Get current users details
+	 * @since 0.1.0
+	 */
+
+	public function tm_hero_get_user() {
+		// Get an instance of the current user
+		$user_id = wp_get_current_user()->ID;
+		$current_user = new WP_User( $user_id );
+		// Set protected variable
+		$this->user = $current_user;
+	}
+
+	/**
+	 * Check users capabilities
+	 * @since 0.1.0
+	 */
+	public function tm_hero_has_user_capability( $capability ) {
+		if ( $this->user->has_cap( $capability ) ) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Hooks to run on WordPress initialization
+	 * @since 0.1.0
+	 */
+	public function tm_hero_init() {
+
+		// @Todo: Check that CM2 is installed
+		add_action(
+			'cmb2_init',
+			array( $this, 'tm_hero_add_fields' )
+		);
+
+	}
+
+	/**
+	 * Only show WordPress update message to administrator roles
+	 * @since 0.1.0
+	 */
+	public function tm_hero_add_fields() {
+
+		$prefix = '_tm_hero_';
+
+		$tm_hero = new_cmb2_box( array(
+			'id'								=> $prefix . 'common',
+			'title'							=> __( 'Hero Banner', 'tm-hero' ),
+			'description'				=> __( 'This controls the large, hero, banner at the top of each page. If a field is not entered the site wide default will be used.', 'tm-hero' ),
+			'object_types'			=> array( 'page', ),
+			'context'						=> 'normal',
+			'priority'					=> 'high',
+			'show_names'				=> 'true',
+		) );
+
+		$tm_hero->add_field( array(
+			'description'				=> __( 'This controls the large, hero, banner at the top of each page. If a field is not entered the site wide default will be used.', 'tm-hero' ),
+			'id'								=> $prefix . 'description' ,
+			'type'							=> 'title',
+		) );
+
+		$tm_hero->add_field( array(
+			'name'							=> __( 'Hide Banner', 'tm-hero' ),
+			'description'				=> __( 'To hide the hero banner on this page. Check the tick box (this will override any global settings).', 'tm-hero' ),
+			'id'								=> $prefix . 'hide' ,
+			'type'							=> 'checkbox',
+		) );
+
+		$tm_hero->add_field( array(
+			'id'								=> $prefix . 'image',
+			'name'							=> __( 'Background', 'tm-hero' ),
+			'desc'							=> 'Select or upload aa background',
+			'type'							=> 'file',
+			'options'						=> array(
+					'url'		=> false,
+			),
+			'text'							=> array(
+					'add_upload_file_text' => 'Add Background',
+			),
+		) );
+
+		$tm_hero->add_field( array(
+			'id'								=> $prefix . 'tagline',
+			'name'							=> __( 'Tagline', 'tm-hero' ),
+			'type'							=> 'textarea_small',
+		) );
+
+		$tm_hero->add_field( array(
+			'id'								=> $prefix . 'btn_text',
+			'name'							=> __( 'Button Text', 'tm-hero' ),
+			'type'							=> 'text_medium',
+		) );
+
+		$tm_hero->add_field( array(
+			'id'								=> $prefix . 'btn_link',
+			'name'							=> __( 'Button Link', 'tm-hero' ),
+			'type'							=> 'text_url',
+			'protocols'					=> array( 'http', 'https', 'mailto' ),
+
+		) );
+
+	}
+
+
+
+}
+
+function tm_hero_init() {
+	TM_Hero::get_instance();
+}
+
+add_action( 'plugins_loaded', 'tm_hero_init' );
+
+function tm_hero_has_hero() {
+	if ( ! get_post_meta( get_the_ID(), '_tm_hero_hide', true ) ) {
+		return true;
+	}
+	return false;
+}
+
+function tm_hero_has_field( $field ) {
+	if ( get_post_meta( get_the_ID(), '_tm_hero_' . sanitize_text_field( $field ), true ) ) {
+		return true;
+	}
+	return false;
+}
+
+function tm_hero_the_image() {
+	$field = get_post_meta( get_the_ID(), '_tm_hero_image_id', true );
+	if ( ! empty( $field ) ) {
+		return $field;
+	}
+	return false;
+}
+
+function tm_hero_the_tagline() {
+	$field = get_post_meta( get_the_ID(), '_tm_hero_tagline', true );
+	if ( ! empty( $field ) ) {
+		return $field;
+	}
+	return false;
+}
+
+function tm_hero_the_btn_link() {
+	$field = get_post_meta( get_the_ID(), '_tm_hero_btn_link', true );
+	if ( ! empty( $field ) ) {
+		return $field;
+	}
+	return false;
+}
+
+function tm_hero_the_btn_text() {
+	$field = get_post_meta( get_the_ID(), '_tm_hero_btn_text', true );
+	if ( ! empty( $field ) ) {
+		return $field;
+	}
+	return false;
+}
