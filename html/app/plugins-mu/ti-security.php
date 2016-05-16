761591932218f388779159700fb9cba7fb5167dd
Michael Bragg <michael@michaelbragg.net>
2016-05-16 14:32:25 +0100

    Add must use plugins

---
+++ html/app/plugins-mu/ti-security.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: TI Security
+ * Plugin URI: https://github.io/thoughtsideas/ti-security/
+ * Description: Improve WordPress security
+ * Author: Michael Bragg
+ * Author URI: http://www.thoughtsandideas.co.uk
+ * Version: 1.0.2
+*/
+
+require_once WPMU_PLUGIN_DIR . '/ti-security/ti-security.php';
